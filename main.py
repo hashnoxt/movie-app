f257816480ea0cd7801ab7d2f3fe874c5ae7dcf8
Hashan Nalaka <hashannalaka0123@gmail.com>
2023-01-08 16:22:52 +0530

    movie-app: initial implementation

---
+++ main.py
@@ -0,0 +1,12 @@
+MENU_PROMPT = "\nEnter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie by title, or 'q' to quit: "
+
+selection = input(MENU_PROMPT)
+while selection != 'q':
+    if selection == 'a':
+        pass
+    elif selection == 'l':
+        pass
+    elif selection == 'f':
+        pass
+    else:
+        print('Unknown command. Please try again.')
